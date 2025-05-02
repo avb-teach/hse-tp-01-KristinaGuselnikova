137e099d75456dcbc6faec9ea917ed81f78da492
Vladislav <vladnsk54@gmail.com>
2025-05-02 16:25:39 +0700

    v1.10

--- kvg.sh
+++ kvg.sh
@@ -19,6 +19,4 @@ if [ ! -d "$output_dir" ]; then
     echo "Выходная директория не существует, будет создана"
     mkdir -p "$output_dir"
 fi
-find "$input_dir" -type f -print0 | while IFS= read -r -d '' file; do
-    cp -- "$file" "$output_dir"
-done
+find "$input_dir" -type f -exec cp -t "$output_dir" -- {} +
